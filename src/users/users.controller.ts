848e1cd72291f8e84ac5d1a94822690b9d0a4ad6
akkassingh <akkassingh@gmail.com>
2025-08-31 21:04:23 -0400

    user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/:id')
+  public getUsers(@Param() params: any, @Query() query: any) {
+    console.log(params.id);
+    console.log(query.limit);
+    return `List of users under id: ${params.id} and limit: ${query.limit}`;
+  }
+  @Post()
+  public createUser(@Body() request: any) {
+    console.log(request);
+    return 'User created!';
+  }
+}
