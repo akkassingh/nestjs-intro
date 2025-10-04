723fb4e822d0319ee616093589fcaeffa8efb5bf
akkassingh <akkassingh@gmail.com>
2025-10-04 15:28:14 -0400

    adding documentation using swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,20 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  // swagger configuration
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS-Intro')
+    .setDescription('baseUrl as http://localhost:3000')
+    .setTermsOfService('http://swagger.io/terms/')
+    .setLicense('MIT', 'https://opensource.org/licenses/MIT')
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .build();
+  // Instantiate swagger document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
