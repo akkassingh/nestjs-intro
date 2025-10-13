cca6f0e876451276f9e0c5047a15bd3116744657
akkassingh <akkassingh@gmail.com>
2025-10-13 16:29:20 -0400

    adding documentation to controller and dto

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreatePostDto } from './dtos/create-post.dto';
+import { PatchPostsDto } from './dtos/patch-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -15,5 +17,25 @@ export class PostsController {
   @Get('{/:userId}') // making parameter optional by adding {}
   public getPosts(@Param('userId') userId: string) {
     return this.postsService.findAll(userId);
+  }
+
+  @ApiOperation({ summary: 'Creates a new post' })
+  @ApiResponse({
+    status: 201,
+    description: 'The post has been created.',
+  })
+  @Post()
+  public createPost(@Body() createPostDto: CreatePostDto) {
+    console.log(createPostDto);
+  }
+
+  @ApiOperation({ summary: 'Updates existing blog post' })
+  @ApiResponse({
+    status: 200,
+    description: 'You get a 200 if the post is updated successfully.',
+  })
+  @Patch('/:id')
+  public updatePost(@Body() patchPostsDto: PatchPostsDto) {
+    console.log('update post', patchPostsDto);
   }
 }
