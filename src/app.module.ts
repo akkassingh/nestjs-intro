c3856180c6bfc5729086fb4b699674a1bfe4af37
akkassingh <akkassingh@gmail.com>
2025-10-19 15:00:28 -0400

    connecting to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        port: 5432,
+        username: 'root',
+        password: 'root',
+        database: 'nestjs-blog',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
